319eb674c5f36393247fb06093dd7bd3d6ad7794
crazyms <crazyms520@hotmail.com>
2015-06-15 12:28:39 +0800

    Ci&Post Text

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -7,10 +7,10 @@ class Welcome extends CI_Controller {
 	 *
 	 * Maps to the following URL
 	 * 		http://example.com/index.php/welcome
-	 *	- or -  
+	 *	- or -
 	 * 		http://example.com/index.php/welcome/index
 	 *	- or -
-	 * Since this controller is set as the default controller in 
+	 * Since this controller is set as the default controller in
 	 * config/routes.php, it's displayed at http://example.com/
 	 *
 	 * So any other public methods not prefixed with an underscore will
@@ -19,8 +19,22 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+		$this->load->helper('url');
 		$this->load->view('welcome_message');
+
+
+
 	}
+	public function post(){
+
+			$num=$_POST['number'];
+			// echo $num;//]
+		$this->load->view('post', array(
+			'num' => $num
+			));
+
+	}
+
 }
 
 /* End of file welcome.php */
