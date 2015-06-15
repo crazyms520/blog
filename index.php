007dc10fa971a65659b7d4e55d41338ba667acfc
crazyms <crazyms520@hotmail.com>
2015-06-15 11:00:17 +0800

    POST text

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-Hi Blog.
+<html>
+  <head>
+    <meta charset="utf-8">
+  <head>
+  <form action="post.php" method="post">
+    輸入數字：
+    <input type="text" name="num">
+    <input type="submit" value="傳送">
+  <body>
+  </body>
+</html>
