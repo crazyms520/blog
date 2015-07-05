56b757536ed4ed55fec377a8d034eb196442092d
crazyms <crazyms520@hotmail.com>
2015-07-05 09:45:22 +0800

    全新ＣＩ

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -2,36 +2,26 @@
 
 class Welcome extends CI_Controller {
 
-	  function __construct(){
-	    parent::__construct();
-	  }
-
-		public function index()
-		{
-			$this->load->helper('cookie');
-
-	    $cookie=$this->input->cookie('is_login');
-	    if(!$cookie){
-	      $has_login = false;
-
-	    	$this->load->view('welcome_message',array(
-	    			'has_login'=>$has_login
-	      	));
-
-	    } else {
-
-	    	if($this->input->cookie('is_login')===$cookie){
-	      $has_login = true ;
-
-	    	}else {
-	      $has_login = false;
-	    }
-
-	    $this->load->view('welcome_message',array(
-	        'has_login'=>$has_login
-	      ));
-		}
-
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('welcome_message');
 	}
-
 }
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
