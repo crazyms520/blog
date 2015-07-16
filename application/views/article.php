8b46e0438dfa4d56428e79565f089bd0c65d0920
crazyms <crazyms520@hotmail.com>
2015-07-16 16:26:33 +0800

    註冊＆文章列表&新增文章..

--- application/views/article.php
+++ application/views/article.php
@@ -15,7 +15,7 @@
   <?php foreach ($articles as $article) { ?>
     <tr>
     <td><b><?php echo $article->id ;?></b></td>
-    <td><b><?php echo $article->nick ;?></b></td>
+    <td><b><?php echo $article->name ;?></b></td>
     <td><b><?php echo $article->title ;?></b></td>
     <td><b><a href='<?php echo site_url('articles/edit/'.$article->id)?>'>修改</a></b></td>
     <td><b><a href='<?php echo site_url('articles/delete/'.$article->id)?>'>刪除</a></b></td>
